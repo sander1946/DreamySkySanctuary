c514f881045828cfcfdb47f4b31dc2412344a9b2
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-11-12 11:18:54 +0100

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,4 +1,5 @@
 import os
+import dotenv
 VERSION: str = '0.0.1'
 APP_NAME: str = 'DreamySkySanctuary'
 DESCRIPTION: str = 'The website of the Dreamy Sky Sanctuary, a discord server about the game Sky: Children of the Light.'
@@ -7,4 +8,5 @@ BASE_DIR: str = os.getcwd()
 TEMPLATES_DIR: str = os.path.join(BASE_DIR, "templates")
 PUBLIC_DIR: str = os.path.join(BASE_DIR, "public")
 STATIC_DIR: str = os.path.join(BASE_DIR, "static")
-TOKEN: str = "NzYzNDI4NzM1MTM0MjAzOTY0.Gtekch.fY85WdWVhyJDHVBgtaZkzr7vY8EMUDVgOfsjkg" #os.getenv("DISCORD_TOKEN")
+ENV_FILE: str = os.path.join(BASE_DIR, ".env")
+TOKEN: str = dotenv.get_key(ENV_FILE, "DISCORD_TOKEN")
