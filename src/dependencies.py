d0e984894f1a03c1fea34ec7d724b0016837b2bf
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-12-02 13:43:29 +0100

    added an upload path

--- src/dependencies.py
+++ src/dependencies.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse, JSONResponse
 from fastapi.concurrency import asynccontextmanager
+from fastapi_utils.tasks import repeat_every
 
 # python imports
 import os
@@ -18,9 +19,14 @@ from src.config import config
 from src.routes.main import main as main_route
 from src.routes.auth import auth as auth_route
 from src.routes.bot import bot as bot_route
+from src.routes.upload import upload as upload_route
 
 # schema imports
 from src.schemas.announcement import Announcement
+from src.schemas.FileTypeException import FileTypeException
+
+# utils imports
+from src.utils.remove_old_uploads import remove_expired_files
 
 # 3rd party imports
 from datetime import datetime, timedelta
